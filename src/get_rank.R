b29af203d5bb07aa667db3f2351aa5a1027de0a9
pandorina1013 <pandorina463@gmail.com>
2018-09-20 11:32:10 +0900

    独自dirを作る

--- src/get_rank.R
+++ src/get_rank.R
@@ -6,6 +6,8 @@ load_packages(c(
 
 # character型の列をキーとしてそれぞれのランキングを出力します。
 get.rank <- function(input.df ,filedir){
+  dir.create(paste0(filedir ,"/rank"))
+  
   colnames.df <- expand.grid(
     character = select_if(input.df, is.character) %>% 
       colnames(),
@@ -29,13 +31,13 @@ get.rank <- function(input.df ,filedir){
         filter_(paste0(chr.col ,"==", "'", dist.name[j,], "'")) %>% 
         mutate_(min_rank=paste0("min_rank(desc(",num.col,"))")) %>% 
         arrange(min_rank) %>% 
-        write.csv(paste0(filedir ,"/choose_", chr.col, "-", dist.name[j,], "_rankby_", num.col, ".csv"))
-      message(paste0(filedir ,"/choose_", chr.col, "-", dist.name[j,], "_rankby_", num.col, ".csv"))
+        write.csv(paste0(filedir ,"/rank/choose_", chr.col, "-", dist.name[j,], "_rankby_", num.col, ".csv"))
+      message(paste0(filedir ,"/rank/choose_", chr.col, "-", dist.name[j,], "_rankby_", num.col))
     }
     input.df %>% 
       mutate_(min_rank=paste0("min_rank(desc(",num.col,"))")) %>% 
       arrange(min_rank) %>% 
-      write.csv(paste0(filedir ,"/choose_all", "_rankby_", num.col, ".csv"), row.names = F)
+      write.csv(paste0(filedir ,"/rank/choose_all", "_rankby_", num.col, ".csv"), row.names = F)
   }
   message("...and all!")
 }
